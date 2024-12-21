89600d769fe59e358be195aec4c5f79b72658c9b
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-21 16:42:09 +0200

    parse items

--- server/app.js
+++ server/app.js
@@ -6,17 +6,70 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const { default: axios } = require('axios');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://zubalana0:zbhQXHED368PbcVK@cluster0.a5jnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(
+    console.log('MongoDB connected')
+)
+
+let itemSchema = new mongoose.Schema({
+    title: String,
+    price: String,
+    status: Boolean
+  });
+  
+let Item = mongoose.model('Item', itemSchema);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const url = 'https://bt.rozetka.com.ua/ua/philips-ep4449-70/p424303650/'
+let url = '';
 
 app.post('/goodsTargetName', async (req, res) => {
     console.log(req.body);
+    url = req.body.URL;
 })
+
+app.get('/itemInfo', async (req, res) => {
+    try {
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const data = [];
+        const titles = [];
+        const statuses = [];
+        $('.title__font').each((index, element) => {
+            const title = $(element).text(); 
+            titles.push(title);
+        });
+
+        $('.status-label').each((index, element) => {
+            const statusText = $(element).text(); 
+            if(statusText.includes('Є в наявності')){
+                statuses.push(true);
+            }else{
+                statuses.push(false);
+            }
+            console.log(statuses)
+        });
+
+        $('.product-price__big').each((index, element) => {
+            const price = $(element).text(); 
+            data.push({ title: titles[index], price, status: statuses[index] });
+            
+        });
+
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+});
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
